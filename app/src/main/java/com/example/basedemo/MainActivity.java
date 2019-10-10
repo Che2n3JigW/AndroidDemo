08a9598a9680685bc7bd1380fad78d478960295b
jChenys <313629058@qq.com>
2019-10-10 16:01:12 +0800

    RecyclerView-添加依赖，自定义Adapter，为Adapter 添加item点击事件接口

--- app/src/main/java/com/example/basedemo/MainActivity.java
+++ app/src/main/java/com/example/basedemo/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.basedemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MyListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        initListener();
+    }
+
+    private void initListener() {
+        adapter.setOnItemClickListener(new MyListAdapter.onItemClickListener() {
+            @Override
+            public void onClick(int position) {
+                switch (position) {
+                    case 0://
+                        
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+            }
+        });
+    }
+
+
+    private void initView() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        String[] strings = new String[]{
+                "Activity",
+                "Service",
+                "Broadcast Receive",
+                "Content Provider"
+        };
+
+        adapter = new MyListAdapter(strings);
+
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/basedemo/MyListAdapter.java
@@ -0,0 +1,68 @@
+package com.example.basedemo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder>
+        {
+
+    private String[] strings;
+    private onItemClickListener itemClickListener;
+
+    //构造函数
+    MyListAdapter(String[] strings) {
+        this.strings = strings;
+    }
+
+    //item点击事件绑定
+    public void setOnItemClickListener(onItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    //绑定视图
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_activity_main, parent, false);
+        return new ViewHolder(view);
+    }
+
+    //绑定数据
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.btn.setText(strings[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return strings.length;
+    }
+
+    //新建ViewHolder
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private final AppCompatButton btn;
+
+        ViewHolder(@NonNull final View itemView) {
+            super(itemView);
+            btn = itemView.findViewById(R.id.btn);
+            btn.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (itemClickListener!=null){
+            itemClickListener.onClick(getAdapterPosition());
+            }
+        }
+    }
+
+    public interface onItemClickListener {
+        void onClick(int position);
+    }
+}
