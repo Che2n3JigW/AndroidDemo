4ce2aebaef16299d2a9baa283142fcb8b77f5369
jChenys <313629058@qq.com>
2019-10-12 18:17:21 +0800

    AnimatorSet(动画集合)、stateListAnimator使用

--- app/src/main/java/com/example/basedemo/animation/AttributeAnimationActivity.java
+++ app/src/main/java/com/example/basedemo/animation/AttributeAnimationActivity.java
@@ -2,8 +2,10 @@ package com.example.basedemo.animation;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.graphics.Path;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,9 +29,44 @@ public class AttributeAnimationActivity extends AppCompatActivity {
 
 //        ValueAnimatorDemo();
 
-        ObjectAnimatorDemo();
+//        ObjectAnimatorDemo();
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            ObjectAnimatorDemo1();
+//        }
+        AnimatorSetDemo();
 
 
+    }
+
+    private void AnimatorSetDemo() {
+        AnimatorSet bouncer = new AnimatorSet();
+        ObjectAnimator objectAnimatorA = ObjectAnimator.ofFloat(textView,"translationX" , 0f, 300f);
+        ObjectAnimator objectAnimatorB = ObjectAnimator.ofFloat(textView, "translationY", 0f, 300f);
+        ObjectAnimator objectAnimatorC = ObjectAnimator.ofFloat(textView, "translationX", 300f, 0f);
+        ObjectAnimator objectAnimatorD = ObjectAnimator.ofFloat(textView, "rotation", 0f, 360f);
+        bouncer.play(objectAnimatorA);
+        bouncer.play(objectAnimatorB).after(objectAnimatorA).with(objectAnimatorD);
+        bouncer.play(objectAnimatorC).after(objectAnimatorB);
+        bouncer.setDuration(2000);
+        bouncer.start();
+//        ValueAnimator fadeAnim = ObjectAnimator.ofFloat(textView, "alpha", 1f, 0f);
+//        fadeAnim.setDuration(2000);
+
+//        AnimatorSet animatorSet = new AnimatorSet();
+//        animatorSet.play(bouncer).before(fadeAnim);
+//        animatorSet.start();
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void ObjectAnimatorDemo1() {//设置两个动画
+        Path path = new Path();
+        path.moveTo(0,1);
+        path.lineTo(300,3);
+        ObjectAnimator.ofFloat(textView,"translationX", "translationY",path)
+                .setDuration(3000)
+                .start();
     }
 
     private void ObjectAnimatorDemo() {
