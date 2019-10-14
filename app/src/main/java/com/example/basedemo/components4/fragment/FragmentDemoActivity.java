ec9b127f54a330c87aad00f1f6959ac93fc7c309
jChenys <313629058@qq.com>
2019-10-14 20:12:37 +0800

    rxjava3、rxAndroid、rxPermission 依赖添加，Loader的简单使用获取通讯录联系人并且查询

--- app/src/main/java/com/example/basedemo/components4/fragment/FragmentDemoActivity.java
+++ app/src/main/java/com/example/basedemo/components4/fragment/FragmentDemoActivity.java
@@ -1,15 +1,40 @@
 package com.example.basedemo.components4.fragment;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.basedemo.R;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
-public class FragmentDemoActivity extends AppCompatActivity implements ArticleFragment.OnArticleCallBack {
+import io.reactivex.disposables.Disposable;
+
+
+public class FragmentDemoActivity
+        extends AppCompatActivity
+        implements ArticleFragment.OnArticleCallBack {
+
+    private ArticleFragment articleFragment;
+    private CursorLoaderListFragment cursorLoaderListFragment;
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+    }
+
+    void showContacts() {
+        CursorLoaderListFragment fragment = new CursorLoaderListFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+        Log.e("===cjw", "???");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +44,31 @@ public class FragmentDemoActivity extends AppCompatActivity implements ArticleFr
             if (savedInstanceState != null) {
                 return;
             }
-            ArticleFragment articleFragment = new ArticleFragment();
+            articleFragment = new ArticleFragment();
+            cursorLoaderListFragment = new CursorLoaderListFragment();
             articleFragment.setArguments(getIntent().getExtras());
             getSupportFragmentManager()
                     .beginTransaction()
-//                    .add(R.id.fragment_container, articleFragment)
+                    .add(R.id.fragment_container, articleFragment)
+                    .add(R.id.fragment_container, cursorLoaderListFragment)
                     .replace(R.id.fragment_container, articleFragment)
                     .commit();
+
+            RxPermissions rxPermissions = new RxPermissions(this);
+
+            Disposable subscribe = rxPermissions
+                    .request(Manifest.permission.READ_CONTACTS,
+                            Manifest.permission.WRITE_CONTACTS)
+                    .subscribe(granted -> {
+                        if (granted) {
+                            // All requested permissions are granted
+                            showContacts();
+                        } else {
+                            // At least one permission is denied
+                            Toast.makeText(this, "拒绝", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
         }
     }
 
@@ -33,7 +76,7 @@ public class FragmentDemoActivity extends AppCompatActivity implements ArticleFr
     public void onArticle(int position) {
         Log.e("===cjw", position + "");
         Fragment fragmentById = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
-        if (fragmentById instanceof ArticleFragment){
+        if (fragmentById instanceof ArticleFragment) {
             ((ArticleFragment) fragmentById).article();
         }
     }
